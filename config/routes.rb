faecc9402b2ec8ef41a5b14e377dbd394e45c4dd
braullio <braulliovg@gmail.com>
2020-03-05 23:49:19 -0300

    update routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users, only: %i[sessions registration passwords]
+
   resources :posts, only: %i[index show new create]
+  resources :users, only: %i[show], param: :username
 end
