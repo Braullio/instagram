8e88b6d44e85ef084692b6dd00b38989aba8bdea
braullio <braulliovg@gmail.com>
2020-03-09 23:23:09 -0300

    add edit_profile and profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create]
   resources :users, only: %i[show], param: :username
+  get 'edit_profile', to: 'users#edit'
+  patch 'profile',    to: 'users#update'
 end
