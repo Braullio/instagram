8f1c882dfeeb473c3a9728c2d9f5669a3f0d9abc
braullio <braulliovg@gmail.com>
2020-03-11 23:24:12 -0300

    using avatar any page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,16 @@ module ApplicationHelper
   def embedded_svg(path)
     File.open("app/assets/images/#{path}", 'rb') { |file| raw(file.read) }
   end
+
+  def user_avatar(user)
+    avatar = if user.avatar.attached?
+               user.avatar
+                   .variant(combine_options: { resize: '100x100^',
+                                               extent: '100x100' })
+             else
+               'default-avatar.jpg'
+             end
+
+    image_tag avatar, class: 'photo'
+  end
 end
